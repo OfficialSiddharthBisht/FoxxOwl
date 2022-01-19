a774ff68b51c58e171efb30d52bb98adacc527e7
SiddharthBisht <officialsiddharthbisht@gmail.com>
2022-01-19 19:58:37 +0530

    minor changes

--- game.js
+++ game.js
@@ -64,7 +64,7 @@ getNewQuestion = () => {
         localStorage.setItem('mostRecentScore', score);
         //go to the end page
         // ! To run in local machine remove /FoxxOwl
-        return window.location.assign('/end.html');
+        return window.location.assign('FoxxOwl/end.html');
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
